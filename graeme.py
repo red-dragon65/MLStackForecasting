f754bea78699d20440679dec4f144911b94264fc
Graeme Beatie <graemebeatie24@outlook.com>
2021-04-20 19:37:34 -0600

    Added the graemeStuff function and the rollingAverage function. The rolling average function takes in a stock df and graphs the average and rolling averages for 5, 10, 50 weeks. This could be altered easily.

--- graeme.py
+++ graeme.py
@@ -12,11 +12,52 @@
 
 
 import pandas as pd
+import DataPreProcessor as dp
+import matplotlib.pyplot as plt
+
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 
-df = pd.read_csv('stocks/Nasdaq-Friday-April-20-2018.csv')
-print(df)
+# df = pd.read_csv('stocks/Nasdaq-Friday-April-20-2018.csv')
+# print(df)
+
+def graemeStuff():
+    nasdaq_df, nyse_df, scap_df, all_stock_df = dp.initializeDf()
+    apple_stock = dp.getStockDfByName('Apple', all_stock_df)
+    # print(apple_stock.head())
+    rollingAverage(apple_stock)
+
+# find the rolling average of a single stock df
+def rollingAverage(stock_df):
+    averageList = []
+    low = stock_df['High']
+    high = stock_df['Low']
+    for x in range(len(stock_df)):
+        avg = (float(high.iloc[x]) + float(low.iloc[x])) / 2
+        averageList.append(avg)
+    Date = stock_df['Date']
+    dict = {'Average': averageList, 'Date': Date}
+    average_df = pd.DataFrame(dict)
+
+    xpoints = stock_df['Date'].to_numpy()
+    ypoints = averageList
+    rollingAvg5 = average_df.rolling(5).mean()
+    rollingAvg10 = average_df.rolling(10).mean()
+    rollingAvg50 = average_df.rolling(50).mean()
+
+    plt.plot(xpoints, ypoints, label="Average")
+    plt.plot(xpoints, rollingAvg5, label="Roll avg 5")
+    plt.plot(xpoints, rollingAvg10, label="Roll avg 10")
+    plt.plot(xpoints, rollingAvg50, label="Roll avg 50")
+
+    # plt.plot(xpoints, high, label="High")
+    # plt.plot(xpoints, low, label="Low")
+    plt.legend()
+    plt.show()
+    # print(averageList)
+    # print(len(averageList))
+    # print(len(stock_df))
 
 def helloWorld():
 
@@ -28,3 +69,4 @@ def helloWorld():
     return value
 
 
+graemeStuff()

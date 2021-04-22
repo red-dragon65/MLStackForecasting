fe25b97bd645d85f52b244f04ff701f0eaa3af1e
Graeme Beatie <graemebeatie24@outlook.com>
2021-04-21 18:00:34 -0600

    Added more to the rawBest function so that I could look at the overall distance between different values.

--- graeme.py
+++ graeme.py
@@ -14,7 +14,7 @@
 import pandas as pd
 import DataPreProcessor as dp
 import matplotlib.pyplot as plt
-
+from operator import itemgetter
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
@@ -67,16 +67,62 @@ def rawBest(big_df):
     # make a dict of the company and its overall change
     # sort this dict
 
-    # set of all the company names
-    compNameSet = set()
+    # all the company names
+    names = big_df['Name']
+    # print(len(names))
+    unique_names = names.unique()
+    # print(len(unique_names))
+    # print(unique_names)
 
     # dict of the company and its overall stock diff
     compDiff = {}
 
     # list to hold the dicts so that it can be sorted by stock diff
+    # count = 0
     compDiffList = []
+    for name in unique_names:
+        stock = big_df.loc[(big_df['Name'] == name)]
+        year_change = stock['YTD % Chg'].iloc[-1]
+        if year_change == '...':
+            year_change = 0
+        year_change = float(year_change)
+        if year_change > 0:
+            open = stock['P/E'].iloc[0]
+            # if open.find(",") != -1:
+            #     while open.find(",") != -1:
+            #         open = open.replace(",", "")
+            if open == '...':
+                open = 0
+            open = float(open)
 
-    print(dp.getStockDfByName("Eyenovia", big_df))
+            close = stock['P/E'].iloc[-1]
+
+            if close == '...':
+                close = 0
+            # if close.find(",") != -1:
+            #     while close.find(",") != -1:
+            #         close = close.replace(",", "")
+            close = float(close)
+            # print(close)
+            # print(type(close))
+            diff = (close - open)
+            # print(diff)
+            if diff > 1000:
+                compDiff = {"Name": name, "Diff": diff}
+                print(compDiff)
+                compDiffList.append(compDiff)
+            # count = count + 1
+            # print(count)
+
+    print(compDiffList)
+    print(len(compDiffList))
+    sorted_list = sorted(compDiffList, key=itemgetter('Diff'),reverse=True)
+    print("Top 3 companies based off of change in P/E")
+    for x in range(3):
+        print(sorted_list[x])
+    # print(compDiffList.)
+
+    # print(dp.getStockDfByName("Eyenovia", big_df))
     # for stock in big_df:
     #     print(stock)
 
@@ -91,4 +137,4 @@ def helloWorld():
     return value
 
 
-graemeStuff()
+# graemeStuff()
